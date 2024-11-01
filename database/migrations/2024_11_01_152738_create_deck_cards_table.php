9f9814ea3918b07a9fa3de124d72d8125d8b2bdb
PeterAjaaa <89441024+PeterAjaaa@users.noreply.github.com>
2024-11-01 22:29:04 +0700

    CODE: Add transactional databases

---
+++ database/migrations/2024_11_01_152738_create_deck_cards_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('deck_cards', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('deck_id')->references('id')->on('decks')->onDelete('cascade');
+            $table->foreignId('card_id')->references('id')->on('cards')->onDelete('cascade');
+            $table->integer('quantity');
+            $table->boolean('is_sideboard');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('deck_cards');
+    }
+};
